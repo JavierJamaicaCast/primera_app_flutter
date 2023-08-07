b00151d28c025d2252dace576b53bc904fc6f920
Javier Jamaica <javiernicolasjamaica@gmail.com>
2023-08-07 16:25:09 +0200

    Cambio de mateapp

--- lib/Clases/Clase02_EstucturaApp/main.dart
+++ lib/Clases/Clase02_EstucturaApp/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MiApp());
+void main() => runApp(MyApp());
 
-class MiApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Material App",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Rutas App Bar"),
+          title: const Text("Material App Bar"),
         ),
-        body: Center(
-            child: Container(
+        body: const Center(
           child: Text("Hello World"),
-        )),
+        ),
       ),
     );
   }
